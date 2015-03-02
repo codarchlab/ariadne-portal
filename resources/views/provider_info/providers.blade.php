33683c57b5c9b7dcbb752ee1b9984158bfee00ca
Olof Olsson <borsna@gmail.com>
2015-03-02 12:46:05 +0100

    Adding routes for listing entities per provider

--- resources/views/provider_info/providers.blade.php
+++ resources/views/provider_info/providers.blade.php
@@ -33,15 +33,63 @@
                                 @foreach ($providers as $provider)
                                 <tr>
                                     <td>{{ $provider->name }}</td>
-                                    <td>{{ $provider->collections }}</td>
-                                    <td>{{ $provider->datasets }}</td>
-                                    <td>{{ $provider->databases }}</td>
+                                    <td>
+                                        @if($provider->collections > 0)
+                                            <a href="{{ action('ProviderController@collection', $provider->id) }}">{{ $provider->collections }}</a>
+                                        @else
+                                            {{ $provider->collections }}
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if($provider->datasets > 0)
+                                            <a href="{{ action('ProviderController@dataset', $provider->id) }}">{{ $provider->datasets }}</a>
+                                        @else
+                                            {{ $provider->datasets }}
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if($provider->databases > 0)
+                                            <a href="{{ action('ProviderController@database', $provider->id) }}">{{ $provider->databases }}</a>
+                                        @else
+                                            {{ $provider->databases }}
+                                        @endif
+                                    </td>
                                     <?php if($full): ?>
-                                    <td>{{ $provider->gis }}</td>
-                                    <td>{{ $provider->schemas }}</td>
-                                    <td>{{ $provider->services }}</td>
-                                    <td>{{ $provider->vocabularies }}</td>
-                                    <td>{{ $provider->foaf }}</td>
+                                    <td>
+                                        @if($provider->gis > 0)
+                                            <a href="{{ action('ProviderController@gis', $provider->id) }}">{{ $provider->gis }}</a>
+                                        @else
+                                            {{ $provider->gis }}
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if($provider->schemas > 0)
+                                            <a href="{{ action('ProviderController@schema', $provider->id) }}">{{ $provider->schemas }}</a>
+                                        @else
+                                            {{ $provider->schemas }}
+                                        @endif
+                                    </td>    
+                                    <td>
+                                        @if($provider->services > 0)
+                                            <a href="{{ action('ProviderController@service', $provider->id) }}">{{ $provider->services }}</a>
+                                        @else
+                                            {{ $provider->services }}
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if($provider->vocabularies > 0)
+                                            <a href="{{ action('ProviderController@vocabulary', $provider->id) }}">{{ $provider->vocabularies }}</a>
+                                        @else
+                                            {{ $provider->vocabularies }}
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if($provider->foaf > 0)
+                                            <a href="{{ action('ProviderController@agent', $provider->id) }}">{{ $provider->foaf }}</a>
+                                        @else
+                                            {{ $provider->foaf }}
+                                        @endif
+                                    </td>
                                     <?php endif;?>
                                 </tr>
                                 @endforeach
