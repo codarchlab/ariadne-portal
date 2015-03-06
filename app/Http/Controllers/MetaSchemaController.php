bc422158dc169ee9d250d483cea1032208e25ee4
DCU <DCU@192.168.10.124>
2015-03-06 16:11:31 +0200

    Added listning of Databases

---
+++ app/Http/Controllers/MetaSchemaController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Services\MetaSchema;
+use Illuminate\Http\Request;
+
+class MetaSchemaController extends Controller {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct() {
+        $this->middleware('guest');
+    }
+
+    /**
+     * Display a listing of the metadata schemas.
+     *
+     * @return Response
+     */
+    public function index() {
+        $metaSchemas= MetaSchema::all();
+        return view('provider_data.metaSchemas')->with('metaSchemas', $metaSchemas);
+    }
+
+    /**
+     * Display the specified metadata schema.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show($id) {
+        $metaSchema = MetaSchema::get($id);
+
+        return view('provider_data.metaSchema')->with('metaSchema', $metaSchema);
+    }
+
+}
