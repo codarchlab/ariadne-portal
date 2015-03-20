10953dfce53c7c8811a59f047d1aa4abf0c92e79
DCU <DCU@192.168.10.98>
2015-03-20 13:58:39 +0200

    Serach results without modal

--- resources/views/search/simpleSearch.blade.php
+++ resources/views/search/simpleSearch.blade.php
@@ -44,7 +44,8 @@
                                             <img src='{{ asset("img/monument.png") }}' height='50' border='0'> 
                                         </div>
                                         <div class='col-md-10'>
-                                            <a href='#' id='dr_item_href' item_id='{{ $dr->id }}' data-toggle='modal' data-target='#item-modal'>{{ $dr->name }}</a>
+                                            <!--<a href='#' id='dr_item_href' item_id='{{ $dr->id }}' data-toggle='modal' data-target='#item-modal'>{{ $dr->name }}</a>-->
+                                            <a href="{{ action('DatabaseController@show', $dr->id) }}">{{ $dr->name }}</a>  
                                         </div>
                                     </div></br>
                                     <div class='row'>
@@ -76,89 +77,7 @@
                     </div>
             </div>
         
-        <!-- ITEM MODAL -->
-        <div class="modal modal-wide fade" id="item-modal" tabindex="-1" role="dialog" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title"><i class="fa  fa-picture-o"></i> <div id="item_title"></div></h4>
-                    </div>
-                        
-                    <div class="modal-body">
-                        <div class="nav-tabs-custom">
-                            <ul id='tabs' class="nav nav-tabs">
-                                    <li class="active"><a href="#tab_1" data-toggle="tab"><i class="fa fa-tag"></i> General</a></li>
-                                    <li class=""><a href="#tab_2" data-toggle="tab"><i class="fa fa-minus-circle"></i> Rights</a></li>
-                            </ul>
-                            <div class="tab-content">
-                                <div class="tab-pane active" id="tab_1">
-
-                                    <div class="row" style="margin-bottom:8px;">
-                                            <div class="col-md-3">Identifier:</div>
-                                            <div class="col-md-9" id="rec-ident">{{ $type['ident'] }}:{{ $dr->id }}</div>
-                                    </div>
-                                    <div class="row" style="margin-bottom:8px;">
-                                            <div class="col-md-3">Title:</div>
-                                            <div class="col-md-9" id="rec-title">{{ $dr->name }}</div>
-                                    </div>
-                                    
-                                    <div class="row" style="margin-bottom:8px;">
-                                            <div class="col-md-3">Description:</div>
-                                            <div class="col-md-9" id="rec-desc">{{ $value }}</div>
-                                    </div>
-                                    
-                                    <div class="row" style="margin-bottom:8px;">
-                                            <div class="col-md-3">Type:</div>
-                                            <div class="col-md-9" id="rec-type">{{ $type['name'] }}</div>
-                                    </div>
-                                    <div class="row" style="margin-bottom:8px;">
-                                            <div class="col-md-3">Language:</div>
-                                            <div class="col-md-9" id="rec-lan"></div>
-                                    </div>
-                                    <div class="row" style="margin-bottom:8px;">
-                                            <div class="col-md-3">Landing Page:</div>
-                                            <div class="col-md-9" id="rec-landingpage"></div>
-                                    </div>
-                                    <div class="row" style="margin-bottom:8px;">
-                                            <div class="col-md-3">Agent:</div>
-                                            <div class="col-md-9" id="rec-agent"></div>
-                                    </div>
-                                    <div class="row" style="margin-bottom:8px;">
-                                            <div class="col-md-3">Ariadne subject:</div>
-                                            <div class="col-md-9" id="rec-ariadne_subject"></div>
-                                    </div>
-                                    <div class="row" style="margin-bottom:8px;">
-                                            <div class="col-md-3">Keywords:</div>
-                                            <div class="col-md-9" id="rec-keywords"></div>
-                                    </div>																													
-                                </div>
-                                <div class="tab-pane " id="tab_2">
-                                        <div class="row" style="margin-bottom:8px;">
-                                                <div class="col-md-3">Publisher:</div>
-                                                <div class="col-md-9" id="rec-publisher"></div>
-                                        </div>
-                                        <div class="row" style="margin-bottom:8px;">
-                                                <div class="col-md-3">Creator:</div>
-                                                <div class="col-md-9" id="rec-creator"></div>
-                                        </div>
-                                        <div class="row" style="margin-bottom:8px;">
-                                                <div class="col-md-3">Owner:</div>
-                                                <div class="col-md-9" id="rec-owner"></div>
-                                        </div>																	
-                                </div>		
-                            </div>
-                        </div>
-
-                    </div>
-
-                <div class="modal-footer clearfix">
-                        <button type="button" class="btn btn-success pull-right" data-dismiss="modal"><i class="fa fa-times"></i> Close</button>
-                </div>
-                        
-                </div><!-- /.modal-content -->
-            </div><!-- /.modal-dialog -->
-        </div><!-- /.modal -->
+      
         
    </section>             
 </aside>
