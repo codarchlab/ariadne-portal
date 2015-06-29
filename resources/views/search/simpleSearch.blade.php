11b1b7cf6d4eb4bc1bf33923fab46c18a4b3d94d
Olof Olsson <borsna@gmail.com>
2015-06-29 17:10:40 +0200

    missing title rendering

--- resources/views/search/simpleSearch.blade.php
+++ resources/views/search/simpleSearch.blade.php
@@ -78,6 +78,8 @@
                                                 <a href="{{ action('GisController@show', $hit['_id']) }}">{{ $hit['_source']['title'] }}</a>
                                             @elseif($hit['_type'] == 'collection')
                                                 <a href="{{ action('CollectionController@show', $hit['_id']) }}">{{ $hit['_source']['title'] }}</a>
+                                            @else
+                                                <a href="#{{ $hit['_type'] }} {{ $hit['_id'] }}">{{ $hit['_source']['title'] }}</a>   
                                             @endif
                                         @else
                                             <a href="#{{ $hit['_type'] }} {{ $hit['_id'] }}">[Title missing]</a>                                  
