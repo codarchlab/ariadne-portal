5cda75ae8928cf20da4593ea2597e12e57886ba3
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2015-08-27 15:53:28 +0200

    navbar and layout realised with plain bootstrap

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,7 +7,6 @@
         <link href="/css/style.css" rel="stylesheet">
 
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
-        <link href="/css/app.css" rel="stylesheet" type="text/css" />
         <link rel="Shortcut icon" href="http://ariadne-infrastructure.eu/extension/mdr_site/design/ariadne/images/favicon.ico" type="image/x-icon" />
         
         <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -18,39 +17,19 @@
         <![endif]-->
     </head>
     <body class="skin-black">
-        <!-- header logo: style can be found in header.less -->
-        <header class="header">
-            <a href="/" class="logo">
-                <!-- Add the class icon to your logo image or logo icon to add the margining -->
-                <img src="{{ asset("img/logo2.png") }}" height="40" style="position:relative; " border="0" />
-            </a>
-            <!-- Header Navbar: style can be found in header.less -->
-            <nav class="navbar navbar-static-top" role="navigation">
-                <!-- Sidebar toggle button-->
-                <a href="#" class="navbar-btn sidebar-toggle" data-toggle="offcanvas" role="button">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </a>
-                <div class="navbar-right">
+        
+        @include('shared._navigation')
 
-
-                </div>
-            </nav>
-        </header>
-        <div class="wrapper row-offcanvas row-offcanvas-left">        
-
-            @include('shared._navigation')
-
+        <div class="container-fluid">
             @yield('content')
+        </div>      
 
-            <script src="https://maps.googleapis.com/maps/api/js?libraries=drawing"></script>
-            <script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/src/markerclusterer.js"></script>
+        <!-- Google Maps libraries -->
+        <script src="https://maps.googleapis.com/maps/api/js?libraries=drawing"></script>
+        <script type="text/javascript" src="http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/src/markerclusterer.js"></script>
 
-
-            <!-- jQuery UI 1.10.3 -->
-            <script src="/js/app.js" type="text/javascript"></script>
+        <!-- combined JS code -->
+        <script src="/js/app.js" type="text/javascript"></script>
                   
     </body>
 </html>
--- resources/views/shared/_navigation.blade.php
+++ resources/views/shared/_navigation.blade.php
@@ -1,124 +1,118 @@
-            <!-- Left side column. contains the logo and sidebar -->
-            <aside class="left-side sidebar-offcanvas">
-                <!-- sidebar: style can be found in sidebar.less -->
-                <section class="sidebar">
+<nav class="navbar navbar-inverse navbar-fixed-top">
 
-                    <br/>
+    <div class="container-fluid">
+    
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="{{ action('WelcomeController@index') }}">
+                <img src="{{ asset("img/logo.png") }}" height="30">
+            </a>
+        </div>
 
-                    <!-- sidebar menu: : style can be found in sidebar.less -->
-                    <ul class="sidebar-menu">
-
-                        <li>
-                            <a href="{{ action('WelcomeController@index') }}">
-                                <i class="fa fa-home"></i> <span>Home</span>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">             
+                <li {{ (Request::is('search') ? 'class=active' : '') }}>
+                    <a href="{{ action('SearchController@index') }}">
+                        <span class="glyphicon glyphicon-search"></span>
+                        Search
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ action('MapController@index') }}">
+                        <span class="glyphicon glyphicon-map-marker"></span>
+                        Map based search
+                    </a>
+                </li>
+                <li {{ (Request::is('provider') ? 'class=active' : '') }}>
+                    <a href="{{ action('ProviderController@index') }}">
+                        <span class="glyphicon glyphicon-user"></span>
+                        Provider info
+                    </a>
+                </li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown"
+                            role="button" aria-haspopup="true"
+                            aria-expanded="false">
+                        <span class="glyphicon glyphicon-list"></span>
+                        Provider data
+                        <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu">
+                       <li{{ (Request::is('collection') ? ' class=active' : '') }}>
+                            <a href="{{ action('CollectionController@index') }}">
+                                <span class="glyphicon glyphicon-folder-close"></span>
+                                Collections
                             </a>
                         </li>
-
-                         <li{{ (Request::is('search') ? ' class=active' : '') }}>
-                            <a href="{{ action('SearchController@index') }}">
-                                <i class="fa fa-search"></i> <span>Search</span>
+                       <li{{ (Request::is('dataset') ? ' class=active' : '') }}>
+                            <a href="{{ action('DatasetController@index') }}">
+                                <span class="glyphicon glyphicon-compressed"></span>
+                                Datasets
                             </a>
-                        </li> 
-
-
-                        <li>
-                            <a href="{{ action('MapController@index') }}">
-                                <i class="fa fa-map-marker"></i> <span>Map based search</span>
+                        </li>                               
+                        <li {{ (Request::is('database') ? ' class=active' : '') }}>
+                            <a href="{{ action('DatabaseController@index') }}">
+                                <span class="glyphicon glyphicon-cloud"></span>
+                                Databases
                             </a>
-                        </li>
-
-                        <li{{ (Request::is('provider') ? ' class=active' : '') }}>
-                            <a href="{{ action('ProviderController@index') }}">
-                                <i class="fa fa-users"></i> <span>Provider info</span>
-                            </a>
-                        </li> 
-
-                        <li class="treeview{{ Utils::containsAndNotContains(Request::url(), array('dataset','textualDocument', 'collection', 'database', 'gis', 'agent', 'metaSchema', 'service', 'vocabulary'), 'search') ? ' active' : '' }}" >
-                            <a href="#">
-                                <i class="fa fa-list"></i> <span>Provider data</span>
-                                <i class="fa fa-angle-left pull-right"></i>
-                            </a>
-                            <ul class="treeview-menu">
-                               <li{{ (Request::is('collection') ? ' class=active' : '') }}>
-                                    <a href="{{ action('CollectionController@index') }}">
-                                        <i class="fa fa-angle-double-right"></i>
-                                        <i class="fa fa-picture-o"></i> <span>Collections</span>
-                                    </a>
-                                </li>
-                               <li{{ (Request::is('dataset') ? ' class=active' : '') }}>
-                                    <a href="{{ action('DatasetController@index') }}">
-                                        <i class="fa fa-angle-double-right"></i>
-                                        <i class="fa fa-archive"></i> <span>Datasets</span>
-                                    </a>
-                                </li>								
-                                <li {{ (Request::is('database') ? ' class=active' : '') }}>
-                                    <a href="{{ action('DatabaseController@index') }}">
-                                        <i class="fa fa-angle-double-right"></i>
-                                        <i class="fa fa-cloud"></i> <span>Databases</span>
-                                    </a>
-                                </li>	
-                               <li{{ (Request::is('gis') ? ' class=active' : '') }}>
-                                    <a href="{{ action('GisController@index') }}">
-                                        <i class="fa fa-angle-double-right"></i>
-                                        <i class="fa fa-globe"></i> <span>GIS</span>
-                                    </a>
-                                </li>
-                               <li{{ (Request::is('textualDocument') ? ' class=active' : '') }}>
-                                    <a href="{{ action('TextualDocumentController@index') }}">
-                                        <i class="fa fa-angle-double-right"></i>
-                                        <i class="fa fa-file"></i> <span>Textual Documents</span>
-                                    </a>
-                                </li>
-                                <!--
-                                <li {{ (Request::is('metaSchema') ? ' class=active' : '') }}>
-                                    <a href="{{ action('MetaSchemaController@index') }}">
-                                        <i class="fa fa-angle-double-right"></i>
-                                        <i class="fa fa-square-o"></i> <span>Metadata Schemas</span>
-                                    </a>
-                                </li>	
-                                <li {{ (Request::is('service') ? ' class=active' : '') }}>
-                                    <a href="{{ action('ServiceController@index') }}">
-                                        <i class="fa fa-angle-double-right"></i>
-                                        <i class="fa fa-gears"></i> <span>Services</span>
-                                    </a>
-                                </li>	
-                                <li {{ (Request::is('vocabulary') ? ' class=active' : '') }}>
-                                    <a href="{{ action('VocabularyController@index') }}">
-                                        <i class="fa fa-angle-double-right"></i>
-                                        <i class="fa fa-file-text-o"></i> <span>Vocabularies</span>
-                                    </a>
-                                </li>
-                               <li{{ (Request::is('agent') ? ' class=active' : '') }}>
-                                    <a href="{{ action('AgentController@index') }}">
-                                        <i class="fa fa-angle-double-right"></i>
-                                        <i class="fa fa-user"></i> <span>Agents</span>
-                                    </a>
-                                </li>-->								
-                            </ul>
-                        </li>    						
-
-                        <li{{ (Request::is('subject') ? ' class=active' : '') }}>
-                            <a href="{{ action('SubjectController@index') }}">
-                                <i class="fa fa-tag "></i> <span>Ariadne subject</span>
-                            </a>
-                        </li>                        
-
-                        <li{{ (Request::is('about') ? ' class=active' : '') }}>
-                            <a href="{{ action('WelcomeController@about') }}">
-                                <i class="fa fa-question-circle"></i> <span>About</span>
+                        </li>   
+                       <li{{ (Request::is('gis') ? ' class=active' : '') }}>
+                            <a href="{{ action('GisController@index') }}">
+                                <span class="glyphicon glyphicon-globe"></span>
+                                GIS
                             </a>
                         </li>
-                        
-                        <li >
-                            <a href="http://www.dcu.gr" target="_blank"><img style="max-width:150px;" src="{{ asset('img/dcu_logo_footer.png') }}" class="img-responsive" /></a>
+                       <li{{ (Request::is('textualDocument') ? ' class=active' : '') }}>
+                            <a href="{{ action('TextualDocumentController@index') }}">
+                                <span class="glyphicon glyphicon-file"></span>
+                                Textual Documents
+                            </a>
                         </li>
-                        
-                        <li >
-                            <a href="http://snd.gu.se" target="_blank"><img style="max-width:150px;" src="{{ asset('img/English_SND.png') }}" class="img-responsive" /></a>
+                        <!--
+                        <li {{ (Request::is('metaSchema') ? ' class=active' : '') }}>
+                            <a href="{{ action('MetaSchemaController@index') }}">
+                                Metadata Schemas
+                            </a>
+                        </li>   
+                        <li {{ (Request::is('service') ? ' class=active' : '') }}>
+                            <a href="{{ action('ServiceController@index') }}">
+                                Services
+                            </a>
+                        </li>   
+                        <li {{ (Request::is('vocabulary') ? ' class=active' : '') }}>
+                            <a href="{{ action('VocabularyController@index') }}">
+                                Vocabularies
+                            </a>
                         </li>
-
+                       <li{{ (Request::is('agent') ? ' class=active' : '') }}>
+                            <a href="{{ action('AgentController@index') }}">
+                                Agents
+                            </a>
+                        </li>-->                                
                     </ul>
+                </li> 
+                <li{{ (Request::is('subject') ? ' class=active' : '') }}>
+                    <a href="{{ action('SubjectController@index') }}">
+                        <span class="glyphicon glyphicon-tags"></span>
+                        Ariadne subject
+                    </a>
+                </li> 
+                <li{{ (Request::is('about') ? ' class=active' : '') }}>
+                    <a href="{{ action('WelcomeController@about') }}">
+                        <span class="glyphicon glyphicon-question-sign"></span>
+                        About
+                    </a>
+                </li>
+            </ul>
 
-                </section>
-                <!-- /.sidebar -->
-            </aside>
+        </div>
+    </div>
+
+</nav>
