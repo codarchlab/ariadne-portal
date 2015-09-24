12f8dfb9f6f3e0a095b43701c3a1eac4f4d84b8b
Jan Wieners <jan@jan-wieners.de>
2015-09-24 17:55:59 +0200

    Assigned identifier to each navbar item to enable detailed styling.

--- resources/views/shared/_navigation.blade.php
+++ resources/views/shared/_navigation.blade.php
@@ -1,10 +1,9 @@
 <nav class="navbar navbar-default navbar-fixed-top">
 
     <div class="container-fluid">
-    
-        <!-- Brand and toggle get grouped for better mobile display -->
+
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -15,104 +14,104 @@
             </a>
         </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">             
-                <li {{ (Request::is('search') ? 'class=active' : '') }}>
-                    <a href="{{ action('SearchController@index') }}">
-                        <span class="glyphicon glyphicon-search"></span>
-                        Search
-                    </a>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav navbar-right">
+                <li id="navSearch" {{ (Request::is('search') ? 'class=active' : '') }}>
+                <a href="{{ action('SearchController@index') }}">
+                    <span class="glyphicon glyphicon-search"></span>
+                    Search
+                </a>
                 </li>
-                <li>
+                <li id="navMap">
                     <a href="{{ action('MapController@index') }}">
                         <span class="glyphicon glyphicon-map-marker"></span>
                         Map based search
                     </a>
                 </li>
-                <li {{ (Request::is('provider') ? 'class=active' : '') }}>
-                    <a href="{{ action('ProviderController@index') }}">
-                        <span class="glyphicon glyphicon-user"></span>
-                        Provider info
-                    </a>
+                <li id="navProviderInfo" {{ (Request::is('provider') ? 'class=active' : '') }}>
+                <a href="{{ action('ProviderController@index') }}">
+                    <span class="glyphicon glyphicon-user"></span>
+                    Provider info
+                </a>
                 </li>
-                <li class="dropdown">
+                <li id="navProviderData" class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown"
-                            role="button" aria-haspopup="true"
-                            aria-expanded="false">
+                       role="button" aria-haspopup="true"
+                       aria-expanded="false">
                         <span class="glyphicon glyphicon-list"></span>
                         Provider data
                         <span class="caret"></span>
                     </a>
                     <ul class="dropdown-menu">
-                       <li{{ (Request::is('collection') ? ' class=active' : '') }}>
-                            <a href="{{ action('CollectionController@index') }}">
-                                <span class="glyphicon glyphicon-folder-close"></span>
-                                Collections
-                            </a>
-                        </li>
-                       <li{{ (Request::is('dataset') ? ' class=active' : '') }}>
-                            <a href="{{ action('DatasetController@index') }}">
-                                <span class="glyphicon glyphicon-compressed"></span>
-                                Datasets
-                            </a>
-                        </li>                               
-                        <li {{ (Request::is('database') ? ' class=active' : '') }}>
-                            <a href="{{ action('DatabaseController@index') }}">
-                                <span class="glyphicon glyphicon-cloud"></span>
-                                Databases
-                            </a>
-                        </li>   
-                       <li{{ (Request::is('gis') ? ' class=active' : '') }}>
-                            <a href="{{ action('GisController@index') }}">
-                                <span class="glyphicon glyphicon-globe"></span>
-                                GIS
-                            </a>
-                        </li>
-                       <li{{ (Request::is('textualDocument') ? ' class=active' : '') }}>
-                            <a href="{{ action('TextualDocumentController@index') }}">
-                                <span class="glyphicon glyphicon-file"></span>
-                                Textual Documents
-                            </a>
-                        </li>
-                        <!--
-                        <li {{ (Request::is('metaSchema') ? ' class=active' : '') }}>
-                            <a href="{{ action('MetaSchemaController@index') }}">
-                                Metadata Schemas
-                            </a>
-                        </li>   
-                        <li {{ (Request::is('service') ? ' class=active' : '') }}>
-                            <a href="{{ action('ServiceController@index') }}">
-                                Services
-                            </a>
-                        </li>   
-                        <li {{ (Request::is('vocabulary') ? ' class=active' : '') }}>
-                            <a href="{{ action('VocabularyController@index') }}">
-                                Vocabularies
-                            </a>
-                        </li>
-                       <li{{ (Request::is('agent') ? ' class=active' : '') }}>
-                            <a href="{{ action('AgentController@index') }}">
-                                Agents
-                            </a>
-                        </li>-->                                
-                    </ul>
-                </li> 
-                <li{{ (Request::is('subject') ? ' class=active' : '') }}>
-                    <a href="{{ action('SubjectController@index') }}">
-                        <span class="glyphicon glyphicon-tags"></span>
-                        Ariadne subject
-                    </a>
-                </li> 
-                <li{{ (Request::is('about') ? ' class=active' : '') }}>
-                    <a href="{{ action('WelcomeController@about') }}">
-                        <span class="glyphicon glyphicon-question-sign"></span>
-                        About
+                        <li{{ (Request::is('collection') ? ' class=active' : '') }}>
+                        <a href="{{ action('CollectionController@index') }}">
+                            <span class="glyphicon glyphicon-folder-close"></span>
+                            Collections
+                        </a>
+                </li>
+                <li{{ (Request::is('dataset') ? ' class=active' : '') }}>
+                <a href="{{ action('DatasetController@index') }}">
+                    <span class="glyphicon glyphicon-compressed"></span>
+                    Datasets
+                </a>
+                </li>
+                <li {{ (Request::is('database') ? ' class=active' : '') }}>
+                <a href="{{ action('DatabaseController@index') }}">
+                    <span class="glyphicon glyphicon-cloud"></span>
+                    Databases
+                </a>
+                </li>
+                <li{{ (Request::is('gis') ? ' class=active' : '') }}>
+                <a href="{{ action('GisController@index') }}">
+                    <span class="glyphicon glyphicon-globe"></span>
+                    GIS
+                </a>
+                </li>
+                <li{{ (Request::is('textualDocument') ? ' class=active' : '') }}>
+                <a href="{{ action('TextualDocumentController@index') }}">
+                    <span class="glyphicon glyphicon-file"></span>
+                    Textual Documents
+                </a>
+                </li>
+                <!--
+                <li {{ (Request::is('metaSchema') ? ' class=active' : '') }}>
+                    <a href="{{ action('MetaSchemaController@index') }}">
+                        Metadata Schemas
                     </a>
                 </li>
+                <li {{ (Request::is('service') ? ' class=active' : '') }}>
+                    <a href="{{ action('ServiceController@index') }}">
+                        Services
+                    </a>
+                </li>
+                <li {{ (Request::is('vocabulary') ? ' class=active' : '') }}>
+                    <a href="{{ action('VocabularyController@index') }}">
+                        Vocabularies
+                    </a>
+                </li>
+               <li{{ (Request::is('agent') ? ' class=active' : '') }}>
+                    <a href="{{ action('AgentController@index') }}">
+                        Agents
+                    </a>
+                </li>-->
+            </ul>
+            </li>
+            <li id="navSubject" {{ (Request::is('subject') ? ' class=active' : '') }}>
+            <a href="{{ action('SubjectController@index') }}">
+                <span class="glyphicon glyphicon-tags"></span>
+                Ariadne subject
+            </a>
+            </li>
+            <li id="navAbout" {{ (Request::is('about') ? ' class=active' : '') }}>
+            <a href="{{ action('WelcomeController@about') }}">
+                <span class="glyphicon glyphicon-question-sign"></span>
+                About
+            </a>
+            </li>
             </ul>
 
         </div>
+
     </div>
 
 </nav>
