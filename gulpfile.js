19e6ec7592a8a8205fb511ed02df10601a758beb
Thomas Kleinke <tkleinke@smail.uni-koeln.de>
2015-10-29 14:36:45 +0100

    adjusted gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,10 @@ var paths = {
 elixir(function(mix) {
     
     // compile sass (including bootstrap)
-    mix.sass('style.scss', 'public/css/', {
+    mix.sass(['style.scss', paths.leaflet + 'css/leaflet.css'], 'public/css/style.css', {
         includePaths: [paths.bootstrap + 'stylesheets/'],
         precision: 8
     });
-
-    // combine css files
-    mix.styles(['public/css/style.css', paths.leaflet + 'css/leaflet.css'], 'public/css/style.css', './');
 
     // copy bootstrap fonts
     mix.copy(paths.bootstrap + 'fonts/bootstrap/**', 'public/fonts/bootstrap/');
