ac06c472ded6116637f11d4f2444c4f705b15742
Jan Wieners <jan@jan-wieners.de>
2015-10-30 15:32:21 +0100

    Made facets collapsible.

--- resources/views/resource/search_facet.blade.php
+++ resources/views/resource/search_facet.blade.php
@@ -1,33 +1,49 @@
 @if(count($buckets) > 0 || Input::has($key))
-    <div class="panel panel-default">
-        <div class="panel-heading">
-            <h3 class="panel-title">{{ trans('resource.'.$key) }}</h3>
-        </div>
-        <div class="list-group">
-            @if(Input::has($key))
-                @foreach(Utils::getArgumentValues($key) as $value)
-                    @if(Utils::keyValueActive($key, $value))
-                        <a href="{{ route('search', Utils::removeKeyValue($key, $value)) }}" class="list-group-item active">
-                            <span class="badge"><span class="glyphicon glyphicon-remove"></span></span>
-                            {{ $value }}
-                        </a>
-                    @endif
-                @endforeach
-            @endif
-      
-            @foreach($buckets as $bucket)
-                @if(Utils::keyValueActive($key, $bucket['key']) == false)
-                    <a href="{{ route('search', Utils::addKeyValue($key, $bucket['key'])) }}" class="list-group-item">
-                        <span class="badge">{{ number_format($bucket['doc_count']) }}</span>
-                        @if(in_array($key, $translateAggregations))
-                            {{ trans('resource.' . $key . '.' . $bucket['key']) }}
-                        @else
-                            {{ $bucket['key'] }}
-                        @endif
+
+    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+
+        <div class="panel panel-default">
+
+            <div class="panel-heading" role="tab" id="heading_{{$key}}">
+                <h3 class="panel-title">
+                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse_{{$key}}" aria-expanded="true" aria-controls="collapse_{{$key}}">
+                        {{ trans('resource.'.$key) }}
                     </a>
-                @endif
-            @endforeach
-        </div>               
-      
+                </h3>
+            </div>
+
+            <div id="collapse_{{$key}}" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading_{{$key}}">
+
+                <div class="list-group">
+
+                    @if(Input::has($key))
+                        @foreach(Utils::getArgumentValues($key) as $value)
+                            @if(Utils::keyValueActive($key, $value))
+                            <a href="{{ route('search', Utils::removeKeyValue($key, $value)) }}" class="list-group-item active">
+                                <span class="badge"><span class="glyphicon glyphicon-remove"></span></span>
+                                {{ $value }}
+                            </a>
+                            @endif
+                        @endforeach
+                    @endif
+
+                    @foreach($buckets as $bucket)
+                        @if(Utils::keyValueActive($key, $bucket['key']) == false)
+                            <a href="{{ route('search', Utils::addKeyValue($key, $bucket['key'])) }}" class="list-group-item">
+                                <span class="badge">{{ number_format($bucket['doc_count']) }}</span>
+                                @if(in_array($key, $translateAggregations))
+                                    {{ trans('resource.' . $key . '.' . $bucket['key']) }}
+                                @else
+                                    {{ $bucket['key'] }}
+                                @endif
+                            </a>
+                        @endif
+                    @endforeach
+                </div>
+
+            </div>
+        </div>
+
     </div>
+
 @endif
