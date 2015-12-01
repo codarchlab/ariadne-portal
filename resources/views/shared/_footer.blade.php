65e49671b14903b2bac2c5cb99417e7397da1843
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2015-12-01 13:58:51 +0100

    hide footer on browse pages (see #4)

--- resources/views/shared/_footer.blade.php
+++ resources/views/shared/_footer.blade.php
@@ -1,11 +1,13 @@
-<footer id="footer">
-    <div class="container-fluid">
-        <p>
+@if (!Request::is('browse/*'))
+	<footer id="footer">
+	    <div class="container-fluid">
+	        <p>
 
-            <img src="{{ asset("img/euflag_footer.jpg") }}" alt="European Commission" id="eulogo">
-            Copyright &copy; 2015 Ariadne. All rights reserved.<br>
-            Ariadne is funded by the <a href="http://ec.europa.eu/research/fp7/index_en.cfm">European Commission's 7th Framework Programme</a>.
+	            <img src="{{ asset("img/euflag_footer.jpg") }}" alt="European Commission" id="eulogo">
+	            Copyright &copy; 2015 Ariadne. All rights reserved.<br>
+	            Ariadne is funded by the <a href="http://ec.europa.eu/research/fp7/index_en.cfm">European Commission's 7th Framework Programme</a>.
 
-        </p>
-    </div>
-</footer>
+	        </p>
+	    </div>
+	</footer>
+@endif
