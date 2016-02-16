84d28c78c841d620082edaacc392051677690c5f
Sebastian Cuy <sebastian.cuy@uni-koeln.de>
2016-02-16 15:09:51 +0100

    remove link to subject landing page on resource page (closes #107)

--- resources/views/resource/page.blade.php
+++ resources/views/resource/page.blade.php
@@ -100,8 +100,8 @@
             @if (isset($resource['_source']['nativeSubject']))
                 <div>
                     @foreach ($resource['_source']['derivedSubject'] as $derivedSubject)
-                        <a class="glyphicon glyphicon-tag" href="{{ route('subject.page', [ 'id' => $derivedSubject['id'] ]) }}"></a>
-                        <a class="tag" href="{{ route('search', [ 'nativeSubject' => $derivedSubject['prefLabel'] ]) }}">                            
+                        <a class="tag" href="{{ route('search', [ 'nativeSubject' => $derivedSubject['prefLabel'] ]) }}">
+                            <span class="glyphicon glyphicon-tag"></span>                  
                             <span itemprop="keywords">{{ $derivedSubject['prefLabel'] }}</span>
                         </a>
                     @endforeach
