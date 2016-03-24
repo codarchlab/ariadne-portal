dbd58f62ec601d78838757d44c97cacfbe2443ef
Olof Olsson <borsna@gmail.com>
2016-03-24 11:36:32 +0100

    #154 testing a fix height for the browse/what page
    should be a dynamic solution

--- resources/views/browse/what.blade.php
+++ resources/views/browse/what.blade.php
@@ -6,10 +6,14 @@
   margin-top: 80px;
   margin-left: 20px;
   margin-right: 20px;
+  height: 100%;
+}
+#wordCloud{
+  height: 500px;
 }
 </style>
 
-<div class='whatBlock'>
+<div class="whatBlock">
   
   <!-- START WORD CLOUD -->
   <div id="wordCloud" class="wordCloudContainerWhat">
