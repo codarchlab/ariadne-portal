944bf1e4456e1239e2028fa9e2494f4a77e60237
Pablo Millet <pablo@dhcp34.snd.gu.se>
2016-06-07 14:42:05 +0200

    Inline css for height

--- resources/views/browse/what.blade.php
+++ resources/views/browse/what.blade.php
@@ -1,6 +1,20 @@
 @extends('app')
 @section('content')
             
+
+<style>
+  html, body {
+    height: 100%;
+  }
+  .wordCloudContainerWhat {
+    height: 95%;
+  }
+  .whatBlock {
+    position: relative;
+    height: 95%;
+  }
+</style>
+
 <div class="whatBlock">
   
   <!-- START WORD CLOUD -->
