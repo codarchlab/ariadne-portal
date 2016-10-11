caefcb572e620df5b77628dcd381388412a08e32
Olof Olsson <olof.olsson@snd.gu.se>
2016-10-11 17:22:01 +0200

    adding schema.org markup for services

--- resources/views/page/service.blade.php
+++ resources/views/page/service.blade.php
@@ -1,18 +1,18 @@
-<div class="row service {{ $type }}">
+<div class="row service {{ $type }}" itemscope="" itemtype="https://schema.org/SoftwareApplication">
     <div class="col-md-4">
         <a href="{{ $service['url'] }}">
-            <img class="img-thumbnail" src="{{ asset("img/services/" . $service['image']) }}" alt="{{ $service['title'] }}" />
+            <img class="img-thumbnail" src="{{ asset("img/services/" . $service['image']) }}" alt="{{ $service['title'] }}" itemprop="image" />
         </a>                      
     </div>
 
     <div class="col-md-8">
-        <h4>{{ $service['title'] }}</h4>
-        <p>
+        <h4 itemprop="name">{{ $service['title'] }}</h4>
+        <p itemprop="description">
           {!! Utils::makeClickableLinks($service['description']) !!}
         </p>
 
         <p>
-            <a href="{{ $service['url'] }}" title="{{ $service['title'] }}">{{ $service['url'] }} <span class="glyphicon glyphicon-new-window" data-toggle="tooltip" data-placement="bottom" title="{{ $service['title'] }}"></span></a>
+            <a href="{{ $service['url'] }}" title="{{ $service['title'] }}" itemprop="sameAs">{{ $service['url'] }} <span class="glyphicon glyphicon-new-window" data-toggle="tooltip" data-placement="bottom" title="{{ $service['title'] }}"></span></a>
         </p>            
     </div>
 </div>
